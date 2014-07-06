f85ec43d21a62029a1a4fbc51fec08fd9d5dee2d
svigano <simo.vigano@gmail.com>
2014-07-06 18:26:44 -0500

    First database sctructure full deployed

---
+++ Server/WattApp.Services/WattApp.Services/Migrations/201407062018101_Initial.cs
@@ -0,0 +1,27 @@
+namespace WattApp.Services.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Guid = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
