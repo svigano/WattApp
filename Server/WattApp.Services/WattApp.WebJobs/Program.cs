d96998f58110003c257e0ab25cb360aa48d5186b
simone vigano <simo.vigano@gmail.com>
2014-08-08 12:56:22 -0500

    add JCI proxy settings. Simplify app.config reads

--- Server/WattApp.Services/WattApp.WebJobs/Program.cs
+++ Server/WattApp.Services/WattApp.WebJobs/Program.cs
@@ -20,8 +20,6 @@ namespace WattApp.WebJobs
     class Program
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
-        private static ITokenProvider _tokenProvider = null;
-        private static string _baseAPIRoot = string.Empty;
 
         static void Main(string[] args)
         {
@@ -32,8 +30,7 @@ namespace WattApp.WebJobs
             try
             {
                 // TO DO Simplify initialization
-                _initClientAPI();
-                APIClient apiClient = new APIClient(_tokenProvider, _baseAPIRoot);
+                var apiClient = _initAPIClient();
 
                 var taskList = new List<ITask>();
                 taskList.Add(new RetrieveDemandTimeSeriesTask(_logger, dataRep, apiClient));
@@ -69,53 +66,22 @@ namespace WattApp.WebJobs
             Thread.Sleep(2000);
         }
 
-        private static void _initClientAPI()
+        private static APIClient _initAPIClient()
         {
-            var clientId = Settings.Get("JciClientId", null, s => !string.IsNullOrEmpty(s), "You must specify a JCI-issued client id in the .config file.");
-            var clientSecret = Settings.Get("JciClientSecret", null, s => !string.IsNullOrEmpty(s), "You must specify a JCI-issued client secret in the .config file.");
+            var clientId = CloudConfigurationManager.GetSetting("JciClientId");
+            var clientSecret = CloudConfigurationManager.GetSetting("JciClientSecret");
             if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(clientSecret))
-                throw new ArgumentException("JciClientId and/or JciClientSecret is not valid. Add them to the .config file settings section.");
-            var tokenEndpoint = Settings.Get("JciTokenEndpoint", "https://identity.johnsoncontrols.com/issue/oauth2/token",
-                s => new Uri(s).IsAbsoluteUri,
-                "JciTokenEndpoint in configuration settings should be the URL where JCI's _tokenProvider are issued from.");
-            _baseAPIRoot = Settings.Get("JciBuildingApiEndpoint", "https://api.panoptix.com/building",
-                s => new Uri(s).IsAbsoluteUri,
-                "JciBuildingApiEndpoint in configuration settings should be the base URL of the Building API.");
+                throw new ArgumentException("JciClientId and/or JciClientSecret is not valid");
 
-            _tokenProvider = new TokenClient(clientId, clientSecret, tokenEndpoint, WebProxy.GetDefaultProxy());
+            var tokenEndpoint = CloudConfigurationManager.GetSetting("JciTokenEndpoint");
+            var baseAPIRoot = CloudConfigurationManager.GetSetting("JciBuildingApiEndpoint");
+
+            if (string.IsNullOrEmpty(tokenEndpoint) || string.IsNullOrEmpty(baseAPIRoot))
+                throw new ArgumentException("API base Urls are not valid");
+
+            var tokenProvider = new TokenClient(clientId, clientSecret, tokenEndpoint, WebProxy.GetDefaultProxy());
+            return new APIClient(tokenProvider, baseAPIRoot);
         }
-    }
-
-    public static class Settings
-    {
-        public static int Get(string settingKey, int defaultValueIfNotFound)
-        {
-            int i = -1;
-            Get(settingKey,
-                defaultValueIfNotFound.ToString(CultureInfo.InvariantCulture),
-                str => Int32.TryParse(str, out i),
-                settingKey + " in config app setting must be an integer.");
-            return i;
-        }
-
-        public static string Get(string settingKey, string defaultValueIfNotFound)
-        {
-            return Get(settingKey, defaultValueIfNotFound, s => true, null);
-        }
-
-        public static string Get(string settingKey, string defaultValueIfNotFound, Predicate<string> validityCheck, string warningMessageIfCheckFails)
-        {
-            var settingValue = GetSettingFromFile(settingKey) ?? defaultValueIfNotFound;
-            if (!validityCheck(settingValue) && !string.IsNullOrEmpty(warningMessageIfCheckFails))
-            {
-                _logger.Warn(warningMessageIfCheckFails);
-            }
-            return settingValue;
-        }
-
-        private delegate string GetSettingDelegate(string key);
-        private static readonly GetSettingDelegate GetSettingFromFile = CloudConfigurationManager.GetSetting;
-        private static Logger _logger = LogManager.GetCurrentClassLogger();
     }
 
 }
