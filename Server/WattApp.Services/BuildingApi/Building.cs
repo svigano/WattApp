68e5c0a735ba276cbcccde0d4d7edcdfae465f0f
simone vigano <simo.vigano@gmail.com>
2014-08-10 11:49:44 -0500

    Add Building and Space entity definition

---
+++ Server/WattApp.Services/BuildingApi/Building.cs
@@ -0,0 +1,26 @@
+﻿using BuildingApi;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WattApp.data.Webjobs.API
+{
+    public class Space : EntityLink
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public EntityLink Type { get; set; }
+        [JsonProperty(PropertyName = "isLocatedInSpace")]
+        public EntityLink Location { get; set; }
+    }
+
+    public class Building : Space
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
